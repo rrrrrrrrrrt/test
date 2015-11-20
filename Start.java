500b228c05c65ad2d8e59f6931898ce57c24ec8c
rrrrrrrrrrt <rrrrrrrrrrt@gmail.com>
2015-11-20 17:33:57 +0200

    first

---
+++ Start.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Start {
+
+
+    static class M1{
+        private Integer a;
+
+        public void setA(Integer a){
+            this.a = a;
+        }
+    }
+
+
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException, IOException {
+        Long l1 = null;
+        long a1 = l1 != null ? l1 : 0;
+        System.out.println("a1="+a1);
+        M1 curM1 = new M1();
+        curM1.setA(5);
+        List<M1> list = new ArrayList<M1>();
+        list.add(curM1);
+        curM1 = new M1();
+        curM1.setA(6);
+        list.toArray();
+
+    }
+
+}
